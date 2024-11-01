825ce95a28cff314689d105315e4b9ea0c75f981
bellobambo <bellobambo21@gmail.com>
2024-11-01 23:59:43 +0100

    custom comonents

--- resources/views/tools/index.blade.php
+++ resources/views/tools/index.blade.php
@@ -6,15 +6,12 @@
     <ul>
         @foreach ($ninjas as $ninja)
             <li>
-                <p>
-                    {{ $ninja['name'] }}
-
-                </p>
-                <a href="/tools/{{ $ninja['id'] }}">
-                    View Details
-                </a>
+                <x-card href="{{ url('/tools/' . $ninja['id']) }}" :highlight="$ninja['skill'] > 70">
+                    <h3>{{ $ninja['name'] }}</h3>
+                </x-card>
             </li>
         @endforeach
     </ul>
+
 
 </x-layout>
